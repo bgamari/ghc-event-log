8b5a45ed015a3a7a6772abb065717edd71876ec4
Ben Gamari <ben@smart-cactus.org>
2015-09-25 21:03:11 +0200

    Main: Use unbuffered output

--- Main.hs
+++ Main.hs
@@ -1,6 +1,8 @@
 import Pipes
 import EventLog
+import System.IO
 
 main = do
+    hSetBuffering stderr NoBuffering
     evlog <- either error id <$> fromFile "test.eventlog"
     runEffect $ for (records evlog) $ \(_,r) -> liftIO $ print r
